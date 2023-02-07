144d4b445cb999c9a6b36b575b4a4a84dd729238
MatthiasMasiero <matthiasmasiero01@gmail.com>
2023-02-07 12:37:31 -0500

    more stuff

---
+++ script.js
@@ -0,0 +1,7 @@
+function backToNormal() {
+    document.getElementById("h").style.backgroundColor = "blue"
+}
+
+function addKetchup() {
+    document.getElementById("image").src = "https://cdn.artphotolimited.com/images/59888232b0ba742a2efde168/1000x1000/alain-prost.jpg"
+}
